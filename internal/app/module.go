633fe2d72046033e820197867d2860b35f26a584
shandysiswandi <shandysiswandi@gmail.com>
2024-09-10 21:19:06 +0700

    add module flag

--- internal/app/module.go
+++ internal/app/module.go
@@ -17,58 +17,66 @@ import (
 )
 
 func (a *App) initModules() {
-	expTodo, err := todo.New(todo.Dependency{
-		Database:       a.database,
-		RedisDB:        a.redisDB,
-		Config:         a.config,
-		UIDNumber:      a.uidNumber,
-		CodecJSON:      a.codecJSON,
-		Validator:      a.validator,
-		ProtoValidator: a.protoValidator,
-		Router:         a.httpRouter,
-		GRPCServer:     a.grpcServer,
-		Logger:         a.logger,
-	})
-	if err != nil {
-		log.Fatalln("failed to init module todo", err)
+	if a.config.GetBool("module.flag.gallery") {
+		_, err := gallery.New(gallery.Dependency{
+			RedisDB:   a.redisDB,
+			Config:    a.config,
+			CodecJSON: a.codecJSON,
+			Validator: a.validator,
+			Router:    a.httpRouter,
+			Logger:    a.logger,
+		})
+		if err != nil {
+			log.Fatalln("failed to init module gallery", err)
+		}
 	}
 
-	_, err = region.New(region.Dependency{
-		Database:  a.database,
-		RedisDB:   a.redisDB,
-		Config:    a.config,
-		CodecJSON: a.codecJSON,
-		Validator: a.validator,
-		Router:    a.httpRouter,
-		Logger:    a.logger,
-	})
-	if err != nil {
-		log.Fatalln("failed to init module region", err)
+	if a.config.GetBool("module.flag.shortly") {
+		_, err := shortly.New(shortly.Dependency{
+			RedisDB:   a.redisDB,
+			Config:    a.config,
+			CodecJSON: a.codecJSON,
+			Validator: a.validator,
+			Router:    a.httpRouter,
+			Logger:    a.logger,
+		})
+		if err != nil {
+			log.Fatalln("failed to init module shortly", err)
+		}
 	}
 
-	_, err = shortly.New(shortly.Dependency{
-		RedisDB:   a.redisDB,
-		Config:    a.config,
-		CodecJSON: a.codecJSON,
-		Validator: a.validator,
-		Router:    a.httpRouter,
-		Logger:    a.logger,
-	})
-	if err != nil {
-		log.Fatalln("failed to init module shortly", err)
+	if a.config.GetBool("module.flag.region") {
+		_, err := region.New(region.Dependency{
+			Database:  a.database,
+			RedisDB:   a.redisDB,
+			Config:    a.config,
+			CodecJSON: a.codecJSON,
+			Validator: a.validator,
+			Router:    a.httpRouter,
+			Logger:    a.logger,
+		})
+		if err != nil {
+			log.Fatalln("failed to init module region", err)
+		}
 	}
 
-	_, err = gallery.New(gallery.Dependency{
-		RedisDB:   a.redisDB,
-		Config:    a.config,
-		CodecJSON: a.codecJSON,
-		Validator: a.validator,
-		Router:    a.httpRouter,
-		Logger:    a.logger,
-	})
-	if err != nil {
-		log.Fatalln("failed to init module gallery", err)
-	}
+	if a.config.GetBool("module.flag.todo") {
+		expTodo, err := todo.New(todo.Dependency{
+			Database:       a.database,
+			RedisDB:        a.redisDB,
+			Config:         a.config,
+			UIDNumber:      a.uidNumber,
+			CodecJSON:      a.codecJSON,
+			Validator:      a.validator,
+			ProtoValidator: a.protoValidator,
+			Router:         a.httpRouter,
+			GRPCServer:     a.grpcServer,
+			Logger:         a.logger,
+		})
+		if err != nil {
+			log.Fatalln("failed to init module todo", err)
+		}
 
-	a.runnables = append(a.runnables, expTodo.Tasks...)
+		a.runnables = append(a.runnables, expTodo.Tasks...)
+	}
 }
