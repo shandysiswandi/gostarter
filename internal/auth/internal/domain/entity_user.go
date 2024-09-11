e8163c0844259e90188da20dacfea18033af5fca
shandysiswandi <shandysiswandi@gmail.com>
2024-09-11 07:46:42 +0700

    draft module auth

---
+++ internal/auth/internal/domain/entity_user.go
@@ -0,0 +1,8 @@
+package domain
+
+type User struct {
+	ID       uint64
+	Username string
+	Email    string
+	Fullname string
+}
---
+++ internal/auth/module.go
@@ -0,0 +1,10 @@
+package auth
+
+type Expose struct{}
+
+type Dependency struct{}
+
+func New(dep Dependency) (*Expose, error) {
+
+	return nil, nil
+}
