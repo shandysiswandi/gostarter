7c1985e92d8714eab7bb08fb60b9466bf6d51a01
shandysiswandi <shandysiswandi@gmail.com>
2025-01-20 07:05:11 +0700

    feat: add web frontend

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <div>Content</div>
+      <div>Footer</div>
+      {/* https://github.com/nyash04/daisyui-landing-page/tree/master */}
+      {/* https://github.com/robbins23/daisyui-admin-dashboard-template */}
+    </>
+  );
+}
---
+++ web/src/components/Header.tsx
@@ -0,0 +1,63 @@
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-50 bg-base-100 py-2">
+      <div className="container">
+        <div className="navbar px-0">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label
+                tabIndex={0}
+                className="btn btn-circle btn-primary mr-1 lg:hidden"
+              >
+                <i className="bi bi-list text-2xl"></i>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu-compact menu dropdown-content mt-1 w-52 rounded-box bg-base-200 p-2 shadow"
+              >
+                <li>
+                  <a href="#!">Home</a>
+                </li>
+                <li>
+                  <a href="#!">Services</a>
+                </li>
+                <li>
+                  <a href="#!">About</a>
+                </li>
+                <li>
+                  <a href="#!">Work</a>
+                </li>
+                <li>
+                  <a href="#!">Case Study</a>
+                </li>
+              </ul>
+            </div>
+            <a className="btn btn-ghost text-2xl normal-case">daisyUI</a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal p-0 font-medium">
+              <li>
+                <a href="#!">Home</a>
+              </li>
+              <li>
+                <a href="#!">Services</a>
+              </li>
+              <li>
+                <a href="#!">About</a>
+              </li>
+              <li>
+                <a href="#!">Work</a>
+              </li>
+              <li>
+                <a href="#!">Case Study</a>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+              <a href="#!">LOGIN</a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
